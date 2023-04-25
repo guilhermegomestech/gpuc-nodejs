9db9228118d63549b9757baeb7f89e16079c9179
Guilherme Gomes <guilhermeg.contato@gmail.com>
2023-04-24 21:46:06 -0300

    Removendo https

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-// require('dotenv').config()
-
+require('dotenv').config()
 const express = require ('express');
-const https = require('https');
-const fs = require('fs');
 const cors = require('cors');
 const path = require ('path')
 const app = express ()
@@ -15,17 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/app', express.static (path.join (__dirname, '/public')))
 
-let port = process.env.PORT || 3000
-// app.listen (port)
+let port = process.env.PORT || 8000
+app.listen (port)
 
 const apiRouter = require('./api/routes/apiRouter')
 app.use ('/api', apiRouter)
 
-
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/$DOMAIN/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/$DOMAIN/fullchain.pem')
-};
-
-// app.listen(8000);
-https.createServer(options, app).listen(8000);
